1ece226513c2a4a5c3489b17fd801db4b329ebfa
Gilles Coomans <gilles.coomans@gmail.com>
2014-01-22 21:31:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ define(["require","deepjs/deep","deepjs/lib/view"], function(require, deep){
 
 	deep.utils.up({
 		deepWidgets : function (descriptor) {
+			var promises = [];
 			var schema ={};
 			descriptor.placed
 			.find("div[deep-widgets]") // <div  object-bind="mp3::id" schema:"jsion::",  maxLength="12"> 
@@ -19,33 +20,36 @@ define(["require","deepjs/deep","deepjs/lib/view"], function(require, deep){
 				var type = $(this).attr("deep-widgets");
 				if(!deep.widgets[type])
 					throw deep.errors.Error(500,"This widget didn't exist yet...");
-
-				deep.widgets[type](descriptor, this);
-
+				promises.push(deep.widgets[type](descriptor, this));
 			});
+			return promises;
 		}		
 	},deep.ui.View.htmlEnhancers);
 
 
 	deep.widgets = {
 		list:function (descriptor,node) {
-			var headerNode = $(node).find("*[list-header]");
-			var headerHtml = headerNode.outerHTML();
-
-			var rowNode = $(node).find("*[list-row]");
-			var rowHtml = rowNode.outerHTML();
-
-			var pagerNode = $(node).find("*[list-pager]");
-			var pagerHtml = pagerNode.outerHTML();
-
-			headerHtml = deep.ui.swig(headerHtml);
-			rowHtml = deep.ui.swig(rowHtml);
-			pagerHtml = deep.ui.swig(pagerHtml);
-
-			$(headerNode).replaceWith(headerHtml({title:"hello"}));
-			$(rowNode).replaceWith(rowHtml({title:"hello"}));
-			$(pagerNode).replaceWith(pagerHtml({title:"hello"}));
-
+			var ressourceURI = $(node).attr("bind");
+			// parse request, take protocol + "schema", get schema as this 
+			return deep.getAll([ressourceURI, schema])
+			.done(function(success){
+				var headerNode = $(node).find("*[list-header]");
+				var headerHtml = headerNode.outerHTML();
+	
+				var rowNode = $(node).find("*[list-row]");
+				var rowHtml = rowNode.outerHTML();
+	
+				var pagerNode = $(node).find("*[list-pager]");
+				var pagerHtml = pagerNode.outerHTML();
+	
+				headerHtml = deep.ui.swig(headerHtml);
+				rowHtml = deep.ui.swig(rowHtml);
+				pagerHtml = deep.ui.swig(pagerHtml);
+	
+				$(headerNode).replaceWith(headerHtml({title:"hello"}));
+				$(rowNode).replaceWith(rowHtml({title:"hello"}));
+				$(pagerNode).replaceWith(pagerHtml({title:"hello"}));	
+			});
 		}
 	};
 });
