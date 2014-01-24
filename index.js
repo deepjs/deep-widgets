fe0e3b5c25807c83de3e4fff7ee0ca6da9650b61
Philippe Delabye <phil@chemicalrules.net>
2014-01-24 11:16:16 +0100

    List widget print rows (range object or array)

--- index.js
+++ index.js
@@ -34,16 +34,22 @@ define(["require","deepjs/deep","deepjs/lib/view"], function(require, deep){
             var schema = request.protocol+"::schema";
 			return deep.getAll([uri, schema])
 			.done(function(result){
-				var datas = result[0], 
-				schema = result[1] || {},
+				var datas = [];
+				console.log("Results = ", result[0]);
+				if(result[0]._deep_range_)
+					datas = result[0].results;
+				else
+					datas = result[0];
+				schema = result[1] || {};
+				console.log("Widget List datas : ", datas); //could receive an array or a simple object or ... depending on the query
+				headerNode = $(node).find("*[list-header]");
+				headerHtml = headerNode.outerHTML();
 				
-				headerNode = $(node).find("*[list-header]"),
-				headerHtml = headerNode.outerHTML(),
+				rowNode = $(node).find("*[list-row]");
+				rowContainer = rowNode.parent();
+				rowHtml = rowNode.outerHTML();
 				
-				rowNode = $(node).find("*[list-row]"),
-				rowHtml = rowNode.outerHTML(),
-				
-				pagerNode = $(node).find("*[list-pager]"),
+				pagerNode = $(node).find("*[list-pager]");
 				pagerHtml = pagerNode.outerHTML();
 				
 				// TO DO : use datas and schema to produce header + rows + pager
@@ -53,7 +59,15 @@ define(["require","deepjs/deep","deepjs/lib/view"], function(require, deep){
 				pagerHtml = deep.ui.swig(pagerHtml);
 	
 				headerNode.replaceWith(headerHtml({title:"hello"}));
-				rowNode.replaceWith(rowHtml({title:"hello"}));
+				
+				rowNode.remove();
+				datas.forEach(function (data) {
+					rowContainer.append(rowHtml(data));
+				});
+				
+				//rowNode.replaceWith(rowHtml({title:"hello"}));
+
+
 				pagerNode.replaceWith(pagerHtml({title:"hello"}));	
 			});
 		}
